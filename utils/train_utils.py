ae893a8dd810bb6bd98f02484ef24253f7e5d851
Samuel Vedrik <samuelvedrik@gmail.com>
2022-04-16 15:57:46 -0400

    Restructured training loop and models

---
+++ utils/train_utils.py
@@ -0,0 +1,58 @@
+import torch
+from tqdm.auto import tqdm
+
+
+def get_accuracy(pred, target):
+    predictions = pred.argmax(axis=1)
+    return (target == predictions).sum() / target.shape[0]
+
+    
+def validation_loop(model, validation_dataloader, criterion, epoch, verbose=False):
+    device = model.device
+    dataset_size = len(validation_dataloader.dataset)
+    running_loss = 0
+    running_accuracy = 0
+    model.eval()
+    # For computational efficiency, we turn off gradients during validation
+    with torch.no_grad():
+        for spectrograms, target in tqdm(validation_dataloader, desc=f"Validation Epoch {epoch}"):
+            spectrograms = spectrograms.to(device)
+            target = target.to(device)
+            pred = model(spectrograms)
+            loss = criterion(pred, target)
+            accuracy = get_accuracy(pred, target)
+            running_loss += loss.item()
+            running_accuracy += accuracy
+    
+    avg_loss = running_loss / dataset_size
+    avg_acc = running_accuracy / dataset_size
+    if verbose:
+        print(f"Epoch {epoch} validation loss: {avg_loss:.3f} | validation acc {avg_acc:.3f}")
+            
+    return avg_loss, avg_acc
+    
+
+def training_loop(model, train_dataloader, criterion, optimizer, epoch, verbose=False):
+    device = model.device
+    dataset_size = len(train_dataloader.dataset)
+    running_loss = 0
+    running_accuracy = 0
+    model.train()
+    for spectrograms, target in tqdm(train_dataloader):
+        spectrograms = spectrograms.to(device)
+        target = target.to(device)
+        pred = model(spectrograms)
+        loss = criterion(pred, target)
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+        running_accuracy += get_accuracy(pred, target)
+    
+    avg_loss = running_loss / dataset_size
+    avg_acc = running_accuracy / dataset_size
+    if verbose:
+        print(f"Epoch {epoch} training loss: {avg_loss:.3f} | training acc {avg_acc:.3f}")
+
+    return avg_loss, avg_acc
