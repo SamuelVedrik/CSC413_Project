34f941ac928798d43c11af2dbec890894858a919
Samuel Vedrik <samuelvedrik@gmail.com>
2022-04-19 11:55:12 -0400

    Modified models to have roughly the same num of params

--- models/convnet.py
+++ models/convnet.py
@@ -9,8 +9,8 @@ class ConvNet(BaseModel):
         self.conv_net = self.build_conv_layers(layer_opts)
         self.flatten = nn.Flatten()
         
-        self.avgpool = nn.AdaptiveAvgPool2d((10, 10))
-        self.fc = nn.Linear(in_features = final_layer_size * 10 * 10, out_features=output_size)
+        self.avgpool = nn.AdaptiveAvgPool2d((5, 5))
+        self.fc = nn.Linear(in_features = final_layer_size * 5 * 5, out_features=output_size)
     
     def forward(self, x):
         x = self.conv_net(x)
--- train.py
+++ train.py
@@ -16,15 +16,15 @@ import os
 ModelClass = MCCRNN
 MODEL_OPTS = dict(
     column1_opts=[
-        dict(in_channels=1, out_channels=64, kernel_size=(3, 13), padding=(1, 6), stride=1),
-        dict(in_channels=64, out_channels=64, kernel_size=(3, 7), padding=(1, 3), stride=1),
+        dict(in_channels=1, out_channels=48, kernel_size=(3, 13), padding=(1, 6), stride=1),
+        dict(in_channels=48, out_channels=64, kernel_size=(3, 7), padding=(1, 3), stride=1),
         ],
     column2_opts=[
-        dict(in_channels=1, out_channels=64, kernel_size=(13, 3), padding=(6, 1), stride=1),
-        dict(in_channels=64, out_channels=64, kernel_size=(7, 3), padding=(3, 1), stride=1),
+        dict(in_channels=1, out_channels=48, kernel_size=(13, 3), padding=(6, 1), stride=1),
+        dict(in_channels=48, out_channels=64, kernel_size=(7, 3), padding=(3, 1), stride=1),
     ],
     combined_opts = [
-        dict(in_channels=128, out_channels=256)
+        dict(in_channels=128, out_channels=256, maxpool_kernel=(2, 4))
     ],
     gru_hidden_size=30,
     output_size=10
